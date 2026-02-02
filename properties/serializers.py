04a058c66f5dcc83e0e8715fd02fe820185555ec
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-02-03 01:20:37 +0200

    Fix: save missing changes in serializers.py

--- properties/serializers.py
+++ properties/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Building, Apartment, Expense, MeterReading
 
+
 class BuildingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Building
