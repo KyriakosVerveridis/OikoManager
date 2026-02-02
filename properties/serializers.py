dc5e26a9b8e907f76e4c5eb923aa9df11e88768d
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2026-02-03 01:09:18 +0200

    feat: Complete API setup for all property models

---
+++ properties/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Building, Apartment, Expense, MeterReading
+
+class BuildingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Building
+        fields = '__all__'
+
+class ApartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Apartment
+        fields = '__all__'
+
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = '__all__'
+
+class MeterReadingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MeterReading
+        fields = '__all__'
